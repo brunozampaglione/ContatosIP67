e81b14fbb17485684c9ccee8b36a656c8082b526
ios5034 <ios5034@Mac-mini-de-caelum-4.local>
2015-03-09 20:36:57 -0300

    Initial Commit

---
+++ ContatosIP67/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ContatosIP67
+//
+//  Created by ios5034 on 09/03/15.
+//  Copyright (c) 2015 ios5034. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ContatosIP67/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ContatosIP67
+//
+//  Created by ios5034 on 09/03/15.
+//  Copyright (c) 2015 ios5034. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
